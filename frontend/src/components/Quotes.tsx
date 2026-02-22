6470a4772a033a805fe1bbffe8fc0be472c98398
eduardcazacu <eduardcazacu@users.noreply.github.com>
2026-02-22 20:16:48 +0100

    Updated quote

--- frontend/src/components/Quotes.tsx
+++ frontend/src/components/Quotes.tsx
@@ -4,14 +4,10 @@ const Quotes = () => {
       <div className="flex justify-center ">
         <div className="max-w-lg">
           <div className="text-3xl font-bold">
-            The customer service I recieved was exceptional. The support team went above and 
-            beyond to address my concerns.
+            I got fed up with the state of social media and did something about it. Come hang out with me in my longe. 
           </div>
           <div className="flex justify max-w-md text-xl font-bold mt-4">
-            Jules Winfield
-          </div>
-          <div className="flex justify max-w-md text-sm font-medium text-slate-700">
-            CEO | Acme
+            Eduard Cazacu
           </div>
         </div>
       </div>
