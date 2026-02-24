683b86b55430a6fbade9c68c33829e61af8b5888
eduardcazacu <eduardcazacu@users.noreply.github.com>
2026-02-24 17:34:47 +0100

    Fixed sad typo in quote

--- frontend/src/components/Quotes.tsx
+++ frontend/src/components/Quotes.tsx
@@ -4,7 +4,7 @@ const Quotes = () => {
       <div className="flex justify-center ">
         <div className="max-w-lg">
           <div className="text-3xl font-bold">
-            I got fed up with the state of social media and did something about it. Come hang out with me in my longe. 
+            I got fed up with the state of social media and did something about it. Come hang out with me in my lounge. 
           </div>
           <div className="flex justify max-w-md text-xl font-bold mt-4">
             Eduard Cazacu
