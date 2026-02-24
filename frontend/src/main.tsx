c166a938b051467fc43363d347de087284326570
eduardcazacu <eduardcazacu@users.noreply.github.com>
2026-02-24 20:20:40 +0100

    analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.tsx'
 import './index.css'
 import { APP_NAME } from './config'
@@ -9,5 +11,7 @@ document.title = APP_NAME;
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 )
